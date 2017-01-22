d52c807fd0aaba957570908a5c5de68f4fbfa16c
Chaker Mallek <chakermellek@gmail.com>
2017-01-22 18:10:04 +0100

    TestDrivenDesign 3

--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -8,10 +8,10 @@ describe('App', () => {
   });
 
   it('should have a title', () => {
-    let subject = browser.getTitle();   
+    let subject = browser.getTitle();
     let result  = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
     expect(subject).toEqual(result);
-  });
+  });   
 
   it('should have `your content here` x-large', () => {
     let subject = element(by.css('[x-large]')).getText();
